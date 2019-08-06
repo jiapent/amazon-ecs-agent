5c0b2a96af21f18a3e439587d32ac27935d4ca1e
Jason Tao <jiapent@amazon.com>
2019-08-06 10:23:08 -0700

    Add auto restart integ test

---
+++ agent/engine/auto_restart_integ_test.go
@@ -0,0 +1,75 @@
+// +build integration
+
+// Copyright 2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package engine
+
+import (
+	"time"
+)
+
+const restartingTimeout = 90 * time.Second
+
+// TestAutoRestartOnFailure is a happy-case integration test that ensure container is restarted.
+// TODO: add StateChange event check when restarting is reported to CP
+func TestAutoRestartOnFailure(t *testing.T) {
+	taskEngine, done, _ := setupWithDefaultConfig(t)
+	defer done()
+
+	stateChangeEvents := taskEngine.StateChangeEvents()
+
+	taskArn := "testAutoRestartOnFailure"
+	testTask := createTestTask(taskArn)
+
+	container1 := createTestContainerWithImageAndName(baseImageForOS, "container1")
+	container2 := createTestContainerWithImageAndName(baseImageForOS, "container2")
+
+	container1.EntryPoint = &entryPointForOS
+	container1.Command = []string{"sleep 15"}
+	container1.Essential = true
+
+	restartMaxAttempts := 3
+	container2.EntryPoint = &entryPointForOS
+	container2.Command = []string{"sleep 2 && exit 1"}
+	container2.Essential = false
+	container2.RestartPolicy = apicontainer.OnFailure
+	container2.RestartMaxAttempts = restartMaxAttempts
+
+	testTask.Containers = []*apicontainer.Container{
+		container1,
+		container2,
+	}
+
+	go taskEngine.AddTask(testTask)
+
+	finished := make(chan interface{})
+	go func() {
+		// Both containers should start
+		verifyContainerRunningStateChange(t, taskEngine)
+		verifyContainerRunningStateChange(t, taskEngine)
+		verifyTaskIsRunning(stateChangeEvents, testTask)
+
+		// After exhausted all retries, container2 is stopped
+		verifyContainerStoppedStateChange(t, taskEngine)
+		verifyContainerStoppedStateChange(t, taskEngine)
+
+		assert.True(t, container2.GetRestartAttempts() > 0, "Did not restarted")
+		assert.Equal(t, restartMaxAttempts, container2.GetRestartAttempts(), "Did not exhaust all restart attempts")
+
+		verifyTaskIsStopped(stateChangeEvents, testTask)
+		close(finished)
+	}()
+
+	waitFinished(t, finished, restartingTimeout)
+}
